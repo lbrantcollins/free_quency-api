831024c57f2d96fdbbaefdece9a1390d9abebe63
Dan <dnshaw12@gmail.com>
2019-08-20 13:16:53 -0500

    server running, models defined

---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask, g 
+from flask_cors import CORS
+from flask_login import LoginManager
+import models
+
+
+DEBUG = True
+PORT = 8000
+
+login_manager = LoginManager()
+
+app = Flask(__name__)
+
+app.config.from_pyfile('./instance/config.py')
+
+app.secret_key = 'SECRET_KEY'
+
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+	try:
+		return models.User.get(models.User.id == user_id)
+	except models.DoesNotExist:
+		return None
+
+@app.before_request
+def before_request():
+	g.db = models.DATABASE
+	g.db.connect()
+
+@app.after_request
+def after_request(response):
+	g.db.close()
+	return response
+
+@app.route('/')
+def index():
+	return 'FREE_QUENCYYYYYYY'
+
+
+if __name__ == '__main__':
+	models.initialize()
+	app.run(debug=DEBUG, port=PORT)
+
+
+
+
+
+
+
---
+++ models.py
@@ -0,0 +1,59 @@
+from peewee import *
+from flask_login import UserMixin
+import datetime
+
+DATABASE = SqliteDatabase('free_quency.sqlite')
+
+class User(UserMixin, Model):
+	username = CharField()
+	email = CharField()
+	password = CharField()
+	image = CharField()
+	about_me = CharField()
+	created_at = DateTimeField(default=datetime.datetime.now)
+
+	class Meta:
+		database = DATABASE
+
+class Media(Model):
+	user_id = ForeignKeyField(User, backref='posts')
+	title = CharField()
+	description = CharField()
+	url = CharField()
+	media_type = CharField()
+	full_html = CharField()
+	thumbnail_html = CharField()
+	created_at = DateTimeField(default=datetime.datetime.now)
+
+	class Meta:
+		database = DATABASE
+
+class Favorite(Model):
+	user_id = ForeignKeyField(User, backref='favorites')
+	media_id = ForeignKeyField(Media, backref='favorites')
+	created_at = DateTimeField(default=datetime.datetime.now)
+
+	class Meta:
+		database = DATABASE
+
+class Comment(Model):
+	user_id = ForeignKeyField(User, backref='comments')
+	media_id = ForeignKeyField(Media, backref='comments')
+	content = CharField()
+	created_at = DateTimeField(default=datetime.datetime.now)
+
+	class Meta:
+		database = DATABASE
+
+def initialize():
+	DATABASE.connect()
+	DATABASE.create_tables([User, Media, Favorite, Comment], safe=True)
+	print('tables created')
+	DATABASE.close()
+
+
+
+
+
+
+
