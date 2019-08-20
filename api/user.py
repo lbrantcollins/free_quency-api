e7b658dc9dac5a2d8e4d87fac59bc124c7daace9
lbrantcollins <lbrantcollins@gmail.com>
2019-08-20 18:37:15 -0500

    auth routes: added documentation and cleaned up code

--- api/user.py
+++ api/user.py
@@ -1,4 +1,4 @@
-import models
+from models import User
 
 import os
 import sys
@@ -12,51 +12,55 @@ from flask_bcrypt import generate_password_hash, check_password_hash
 from flask_login import login_user, current_user
 from playhouse.shortcuts import model_to_dict
 
+# set default url path for users (register blueprint in app.py)
 user = Blueprint('users', 'user', url_prefix='/user')
 
 def save_picture(form_picture):
 	print(form_picture)
 
-
+	# encode file as a string for storage in the database
 	str = base64.b64encode(form_picture['file'].read())
 
 	print(str)
 
 
-
+# Register (POST) route
+####################################################
 @user.route('/register', methods=['POST'])
 def register():
 
 	print('REGISTER HIT')
 
-	pay_file = request.files
-
+	# multipart form data (text fields and an image file)
 	payload = request.form.to_dict()
-	dict_file = pay_file.to_dict()
 
+	pay_file = request.files
+	dict_file = pay_file.to_dict()
 	save_picture(dict_file)
+
+
 
 	# payload['email'].lower()
 
 	# try:
 
-	# 	models.User.get(models.User.email == payload['email'])
+	# 	User.get(User.email == payload['email'])
 
 	# 	return jsonify(data={}, status={'code': 401, 'message': 'A User with that email already exists'})
 
-	# except models.DoesNotExist:
+	# except DoesNotExist:
 
 	# 	try:
 
-	# 		models.User.get(models.User.username == payload['username'])
+	# 		User.get(User.username == payload['username'])
 
 	# 		return jsonify(data={}, status={'code': 401, 'message': 'A User with that username already exists'})
 
-	# 	except models.DoesNotExist:
+	# 	except DoesNotExist:
 
 	# 		payload['password'] = generate_password_hash(payload['password'])
 
-	# 		user = models.User.create(**payload)
+	# 		user = User.create(**payload)
 
 	# 		login_user(user)
 
@@ -66,6 +70,7 @@ def register():
 
 	# 		return jsonify(data=user_dict, status={'code': 201, 'message': 'Success'}) 
 
+# Login (GET) route
 @user.route('/login', methods=['GET'])
 def login():
 
@@ -73,17 +78,18 @@ def login():
 
 	try:
 
-		user = models.User.get(models.User.username == payload['username'])
+		user = User.get(User.username == payload['username'])
 
 		passwordCheck = check_password_hash(user.password, payload['password'])
 
 		print(passwordCheck, 'passwordCheck')
 
 		if not passwordCheck:
+			# we reach this point if neither username nor pwd correct
 			return jsonify(data={}, status={'code': 401, 'message': 'Username or password is incorrect.'})
 
 		else:
-
+			# we only reach this point if both username and pwd correct
 			login_user(user)
 
 			user_dict = model_to_dict(user)
@@ -93,9 +99,10 @@ def login():
 			return jsonify(data=user_dict, status={'code': 200, 'message': 'user logged in!'})
 
 
-	except models.DoesNotExist:
+	except DoesNotExist:
 
-		return jsonify(data={}, status={'code': 401, 'message': 'No user exists with that username'})
+		# we reach this point if username does not exist
+		return jsonify(data={}, status={'code': 401, 'message': 'Username or password is incorrect.'})
 
 
 
