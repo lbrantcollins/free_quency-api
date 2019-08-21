2bb77b9ad1a15325af970f1d91c664e67075b2e1
lbrantcollins <lbrantcollins@gmail.com>
2019-08-21 14:42:46 -0500

    favorites create route tested and debugged

--- api/favorite.py
+++ api/favorite.py
@@ -1,6 +1,7 @@
 from models import Favorite
 
 from flask import Blueprint, request, jsonify, url_for, send_file
+from flask_login import current_user
 
 from playhouse.shortcuts import model_to_dict
 
@@ -10,12 +11,13 @@ favorite = Blueprint('favorites', 'favorite', url_prefix='/favorite')
 # create/POST favorite
 ###############################################
 @favorite.route('/', methods=['POST'])
-def create_favorite:
+def create_favorite():
 
 	# must send media id with payload when favorite button clicked
-	# payload includes media_id and timestamp
+	# the following assumes payload includes media_id and timestamp
 	payload = request.form.to_dict()
-	payload['user_id'] = current_user
+	user_id = model_to_dict(current_user)['id']
+	payload['user_id'] = user_id
 
 	favorite = Favorite.create(**payload)
 
@@ -23,6 +25,23 @@ def create_favorite:
 
 	return jsonify(data=favorite_dict, status={"code": 200, 
 		"message": "Resource added"})
+
+# DELETE a favorite ("un-favorite" a media entry)
+@favorite.route('/', methods=['DELETE'])
+def delete_favorite():
+
+	# must send media id with payload when favorite button clicked
+	# the following assumes payload includes media_id
+	payload = request.form.to_dict()
+	user_id = model_to_dict(current_user)['id']
+
+	query = Favorite.delete().where(
+		Favorite.user_id == user_id and 
+		Favorite.media_id == payload['media_id']
+		)
+	query.execute()
+
+
 
 
 
